959f2d49d921dd4bbef789e026232691eacda26e
DanielRojoR <danielrojor1999@gmail.com>
2025-03-06 10:39:55 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,50 +1,62 @@
-let currentIndex = 0;
+document.addEventListener('DOMContentLoaded', function () {
+    class Carousel {
+        constructor(container) {
+            this.container = container;
+            this.slider = container.querySelector('.slider');
+            this.slides = container.querySelectorAll('.slide');
+            this.prevBtn = container.querySelector('.prev');
+            this.nextBtn = container.querySelector('.next');
+            this.dots = container.parentElement.querySelectorAll('.dot');
+            this.currentIndex = 0;
+            this.updateDots();
+            this.addEventListeners();
+        }
 
-function moveSlides(x) {
-    let carouselItems = document.getElementsByClassName("carousel-img");
-    let totalItems = carouselItems.length;
-    
-    currentIndex += x;
+        updateDots() {
+            this.dots.forEach((dot, index) => {
+                if (index === this.currentIndex) {
+                    dot.classList.add('active');
+                } else {
+                    dot.classList.remove('active');
+                }
+            });
+        }
 
-    if (currentIndex <= -1) {
-        currentIndex = 2
-    } else if (currentIndex >= 3) {
-        currentIndex = 0
+        showSlides(index) {
+            if (index >= this.slides.length) {
+                this.currentIndex = 0;
+            } else if (index < 0) {
+                this.currentIndex = this.slides.length - 1;
+            } else {
+                this.currentIndex = index;
+            }
+            this.slider.style.transform = `translateX(-${this.currentIndex * 100}%)`;
+            this.updateDots();
+        }
+
+        nextSlide() {
+            this.showSlides(this.currentIndex + 1);
+        }
+
+        prevSlide() {
+            this.showSlides(this.currentIndex - 1);
+        }
+
+        addEventListeners() {
+            this.dots.forEach((dot, index) => {
+                dot.dataset.index = index;
+                dot.addEventListener('click', () => {
+                    this.showSlides(parseInt(dot.dataset.index, 10));
+                });
+            });
+
+            this.nextBtn.addEventListener('click', () => this.nextSlide());
+            this.prevBtn.addEventListener('click', () => this.prevSlide());
+        }
     }
 
-    for (let i = 0; i < totalItems; i++) {
-        carouselItems[i].style.display = "none";
-    }
-    carouselItems[currentIndex].style.display = "block";
-}
-
-function moveSlidesIndicator(x) {
-    let carouselItems = document.getElementsByClassName("carousel-img");
-    let totalItems = carouselItems.length;
-    currentIndex = 0;
-
-    currentIndex += x;
-
-    for (let i = 0; i < totalItems; i++) {
-        carouselItems[i].style.display = "none";
-    }
-    carouselItems[currentIndex].style.display = "block";
-    console.log(currentIndex);
-}
-
-// function animateSlide() {
-//     let id = null;
-//     const elem = document.getElementsByClassName("active");
-//     let pos = 0;
-//     clearInterval(id);
-//     id = setInterval(frame, 5);
-//     function frame() {
-//       if (pos == 350) {
-//         clearInterval(id);
-//       } else {
-//         pos++;
-//         elem.style.top = pos + 'px';
-//         elem.style.left = pos + 'px';
-//       }
-//     }
-//   } 
+    // Initialize all carousels on the page
+    document.querySelectorAll('.slider-container').forEach(container => {
+        new Carousel(container);
+    });
+});
